3c62305b6a6aa8314e2e2af0291d43aeab0959c0
paccao <16489683+paccao@users.noreply.github.com>
2022-01-21 11:24:25 +0100

    smoke test for component url render

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,18 @@
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('App component renders without crashing', () => {
-    render(<App />);
+describe('App component', () => {
+    it(' renders without crashing', () => {
+        render(<App />);
+    });
+
+    it('meetup-list page component renders at the corresponding url', () => {
+        render(<App />);
+        const pathname = '/';
+    });
+
+    it('meetup page component renders at the corresponding url', () => {
+        render(<App />);
+        const pathname = '/meetup';
+    });
 });
