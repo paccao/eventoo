6ef0e976ba84906cc9f91cf9d4a155b33d05d674
krispad-dev <kristofer.padoan@icloud.com>
2022-01-21 17:45:59 +0100

    first MeetUpListItem tests complete

--- src/components/meetupList/test/MeetUpListItem.test.tsx
+++ src/components/meetupList/test/MeetUpListItem.test.tsx
@@ -9,31 +9,54 @@ it('should render', () => {
     render(<MeetUpListItem />);
 });
 
-/* it('should link to a detailed meetup page', () => {
+it('should link to a detailed meetup page', () => {
     render(<MeetUpListItem />);
 
-}); */
+});
 
 it('should be clickable', () => {
     render(<MeetUpListItem />);
 
-    
-    
+    const listItem = screen.getAllByRole('listitem')
 
+    expect(listItem).toHaveStyle('cursor: pointer')
 });
+
 
 it('should have date and location info', () => {
     render(<MeetUpListItem />);
 
-    const title = scrren.ge
+    const text = screen.getAllByRole('heading', { name: /göteborg/i })
+
+    expect(text).toHaveTextContent('göteborg')
+    expect(text).toHaveTextContent(/2020/i)
 });
 
+
 it('should have title present', () => {
-    render(<MeetUpListItem />);
+    render(<MeetUpListItem />)
+
+    const text = screen.getAllByRole('heading', { name: 'lördag på landet' })
+
+    expect(text).toHaveTextContent('göteborg')
 });
+
 
 it('should have type tag present', () => {
     render(<MeetUpListItem />);
+
+    const tag = screen.getByAltText('outdoors')
+
+    expect(tag).toBeInTheDocument()
+});
+
+
+it('should have image present', () => {
+    render(<MeetUpListItem />);
+
+    const image = screen.getByRole('img')
+
+    expect(image).toBeInTheDocument()
 });
 
 
