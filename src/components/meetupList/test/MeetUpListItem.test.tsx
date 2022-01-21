4ee1f54034e378e21e5dbfd1375f71e8ba808b7c
krispad-dev <kristofer.padoan@icloud.com>
2022-01-21 15:46:46 +0100

    started testing MeetUpListItem

---
+++ src/components/meetupList/test/MeetUpListItem.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import MeetUpListItem from '../MeetUpListItem'
+
+
+
+it('should render', () => {
+    render(<MeetUpListItem />);
+});
+
+/* it('should link to a detailed meetup page', () => {
+    render(<MeetUpListItem />);
+
+}); */
+
+it('should be clickable', () => {
+    render(<MeetUpListItem />);
+});
+
+it('should have date and location info', () => {
+    render(<MeetUpListItem />);
+
+    const title = scrren.ge
+});
+
+it('should have title present', () => {
+    render(<MeetUpListItem />);
+});
+
+it('should have type tag present', () => {
+    render(<MeetUpListItem />);
+});
+
+
