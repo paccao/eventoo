86f6e4cbe63ec547bc791075b6dde28376ee28f9
Adam-Bjurhede <adambjurhede@gmail.com>
2022-01-23 20:40:10 +0100

    file was formatted on save

--- src/db/Db.ts
+++ src/db/Db.ts
@@ -1,24 +1,18 @@
 import { Meeting } from '../context/AppState';
 
 export class Db {
-
-    setMeetup(meetup: Meeting[] ): void {
-
-        const meetupString = JSON.stringify(meetup)
-        localStorage.setItem('meetup', meetupString)
-
+    setMeetup(meetup: Meeting[]): void {
+        const meetupString = JSON.stringify(meetup);
+        localStorage.setItem('meetup', meetupString);
     }
 
     getMeetup(): Meeting[] {
-
-        if (localStorage.getItem("meetup") === null) {
-            return []
-          } else {
-            return JSON.parse(localStorage.getItem('meetup') || '')
-
-
-          }
+        if (localStorage.getItem('meetup') === null) {
+            return [];
+        } else {
+            return JSON.parse(localStorage.getItem('meetup') || '');
+        }
     }
 }
 
-const db = new Db()
+const db = new Db();
