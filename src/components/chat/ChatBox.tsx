1ffde85673c44f9eacca60cf18abe20ee4acf8db
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-01-25 13:57:52 +0100

    styled chat box component

--- src/components/chat/ChatBox.tsx
+++ src/components/chat/ChatBox.tsx
@@ -1,15 +1,27 @@
 import ChatMessageList from './ChatMessageList';
 import ChatSubmitForm from './ChatSubmitForm';
 import InfoBlockDivider from '../globals/InfoBlockDivider';
+import styled from 'styled-components';
 
 function ChatBox() {
     return (
-        <div>
+        <Wrapper>
             <InfoBlockDivider text="Diskussion" />
             <ChatMessageList />
             <ChatSubmitForm />
-        </div>
+        </Wrapper>
     );
 }
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 1rem;
+
+    > :not(:first-child) {
+        margin-left: 1rem;
+    }
+`;
 
 export default ChatBox;
