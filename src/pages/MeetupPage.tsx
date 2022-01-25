bd6830cf823f7a5dcf70ec9268d857918220e587
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-01-25 13:41:02 +0100

    basic responsive layout for the meetup page

--- src/pages/MeetupPage.tsx
+++ src/pages/MeetupPage.tsx
@@ -1,11 +1,19 @@
 import ChatBox from '../components/chat/ChatBox';
+import styled from 'styled-components';
 
 function MeetupPage() {
     return (
-        <div>
+        <PageWrapper>
             <ChatBox />
-        </div>
+        </PageWrapper>
     );
 }
+
+const PageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 1rem;
+`;
 
 export default MeetupPage;
