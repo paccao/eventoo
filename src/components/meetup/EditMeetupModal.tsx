6824bf76ca3195cb4c90ba6887f582502ac8cacd
Adam-Bjurhede <adambjurhede@gmail.com>
2022-01-28 12:10:18 +0100

    working on edit meeting

---
+++ src/components/meetup/EditMeetupModal.tsx
@@ -0,0 +1,240 @@
+import React, { useContext, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { UiContext } from '../../context/UiState';
+import { AppContext } from '../../context/AppState';
+import { MdOutlineLaptopMac } from 'react-icons/md';
+
+import { Meeting } from '../../context/AppState';
+import { nanoid } from 'nanoid';
+
+interface Props {
+    currentMeetup: Meeting;
+}
+
+function EditMeetupModal({ currentMeetup }: Props) {
+    console.log(currentMeetup?.location);
+
+    const today = new Date().toLocaleDateString('sv-se');
+
+    const { dispatch } = useContext(UiContext);
+    const { dispatch: appDispatch } = useContext(AppContext);
+
+    const [title, setTitle] = useState<string>(currentMeetup?.title);
+    const [tag, setTag] = useState<string>(currentMeetup?.tag[0]);
+    const [image, setImage] = useState<string>(currentMeetup?.image);
+    const [location, setLocation] = useState<string>(currentMeetup?.location);
+    const [date, setDate] = useState<string>(currentMeetup?.time);
+    const [time, setTime] = useState<string>('18:00');
+    const [isOnline, setIsOnline] = useState<boolean>(false);
+
+    //Togles value of location input value when isOnline button is pressed
+    useEffect(() => {
+        isOnline && setLocation('online');
+    }, [isOnline]);
+
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        if (!title || !tag || !image || !location || !date || !time) {
+            return;
+        }
+
+        const dateAndTime = date + ' ' + time;
+
+        const meeting: Meeting = {
+            id: nanoid(),
+            title,
+            tag: [tag],
+            time: dateAndTime,
+            isOnline,
+            location,
+            image,
+            timeStamp: Date.parse(dateAndTime),
+            comments: [],
+        };
+
+        appDispatch({ type: 'ADD_MEETUP', payload: meeting });
+        dispatch({ type: 'TOGGLE_CREATE_MEETING_MODAL' });
+
+        setLocation('');
+    }
+
+    return (
+        <EditMeetupModalContainer>
+            <form onSubmit={handleSubmit} className="meeting-form">
+                <div className="close-container">
+                    <h2>Ändra meetup..</h2>{' '}
+                    <div
+                        className="x"
+                        onClick={() => dispatch({ type: 'TOGGLE_SHOW_EDIT_DELETE_MEETING_MODAL' })}
+                    >
+                        {' '}
+                        X
+                    </div>
+                </div>
+                <input
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                    type="text"
+                    id="title"
+                    name="title"
+                    placeholder="titel:"
+                    required
+                />
+                <input
+                    onChange={(e) => setTag(e.target.value)}
+                    value={tag}
+                    type="text"
+                    id="ämne"
+                    name="ämne"
+                    placeholder="ämne:"
+                    required
+                />
+                <input
+                    onChange={(e) => setImage(e.target.value)}
+                    value={image}
+                    type="text"
+                    id="bild"
+                    name="bild"
+                    placeholder="bild:"
+                    required
+                />
+                <div className="date-time-container">
+                    <input
+                        onChange={(e) => setDate(e.target.value)}
+                        value={date}
+                        type="date"
+                        id="date"
+                        name="date"
+                        placeholder="date:"
+                        min={today}
+                        required
+                    />
+                    <input
+                        onChange={(e) => setTime(e.target.value)}
+                        value={time}
+                        type="time"
+                        id="time"
+                        name="time"
+                        placeholder="time:"
+                        required
+                    />
+                </div>
+                <div className="place-container">
+                    <input
+                        onChange={(e) => setLocation(e.target.value)}
+                        value={location}
+                        type="text"
+                        id="plats"
+                        name="plats"
+                        placeholder="plats:"
+                        required
+                        disabled={isOnline}
+                    />
+                    <button
+                        onClick={() => setIsOnline(!isOnline)}
+                        type="button"
+                        className={`isOnline-button ${isOnline ? 'online' : null}`}
+                    >
+                        <MdOutlineLaptopMac />
+                    </button>
+                </div>
+
+                <button className="submit-button" type="submit">
+                    UPPDATERA
+                </button>
+            </form>
+        </EditMeetupModalContainer>
+    );
+}
+
+export default EditMeetupModal;
+
+const EditMeetupModalContainer = styled.section`
+    position: fixed;
+    inset: 0 0 0 0;
+    background-color: ${(props) => props.theme.__bgColorLargePageWidth};
+    opacity: 0.95;
+    z-index: 1;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+
+    .meeting-form {
+        width: 100%;
+        max-width: 300px;
+        display: flex;
+        flex-direction: column;
+
+        .close-container {
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-bottom: 1.5rem;
+
+            .x {
+                color: ${(props) => props.theme.accentColorAdmin};
+                font-size: 1.2rem;
+                font-weight: bold;
+            }
+
+            .x:hover {
+                font-size: 1.3rem;
+                cursor: pointer;
+            }
+        }
+
+        input {
+            margin-bottom: 1.5rem;
+            border-radius: ${(props) => props.theme.borderRadius};
+            border: none;
+            padding: 10px;
+            background-color: ${(props) => props.theme.cardBgColor};
+            color: ${(props) => props.theme.textColor};
+        }
+
+        input::placeholder {
+            color: ${(props) => props.theme.textColor};
+        }
+
+        .submit-button {
+            border-radius: ${(props) => props.theme.borderRadius};
+            border: none;
+            padding: 7px;
+            background-color: ${(props) => props.theme.accentColorAdmin};
+            font-size: 1.5rem;
+            font-weight: bold;
+        }
+    }
+
+    .place-container {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+
+        .isOnline-button {
+            background-color: ${(props) => props.theme.cardBgColor};
+            border-radius: ${(props) => props.theme.borderRadius};
+            color: ${(props) => props.theme.accentColorAdmin};
+            font-size: 1rem;
+            border: none;
+            padding: 0.55rem;
+        }
+
+        .online {
+            background-color: ${(props) => props.theme.accentColorAdmin};
+            color: ${(props) => props.theme.cardBgColor};
+        }
+    }
+
+    .date-time-container {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-start;
+    }
+`;
--- src/components/meetup/Meetup.tsx
+++ src/components/meetup/Meetup.tsx
@@ -4,71 +4,77 @@ import MeetupBtnsContainer from './MeetupBtnsContainer';
 import { nanoid } from 'nanoid';
 
 import TagChip from '../globals/TagChip';
+import EditMeetupModal from './EditMeetupModal';
+import { useContext } from 'react';
+import { UiContext } from '../../context/UiState';
 
 interface Props {
-	currentMeetup: Meeting;
+    currentMeetup: Meeting;
 }
 
 function Meetup({ currentMeetup }: Props) {
-	return (
-		<MeetingContainer>
-			<section
-				role='img'
-				className='landing'
-				style={{ backgroundImage: `url(${currentMeetup && currentMeetup?.image})` }}
-			>
-				<h1>{currentMeetup?.title}</h1>
-			</section>
-			<section className='content-container'>
-				<section className='location-time'>
-					<code>{currentMeetup && currentMeetup?.time}</code>
-					<h2>{currentMeetup?.location}</h2>
-					<MeetupTagsContainer>
-						{currentMeetup?.tag.map(tag => (
-							<TagChip key={nanoid()} text={tag} />
-						))}
-					</MeetupTagsContainer>
-				</section>
-				<section className='interact-section'>
-					<MeetupBtnsContainer
-						id={currentMeetup?.id}
-						date={currentMeetup && currentMeetup.time}
-					/>
-				</section>
-			</section>
-		</MeetingContainer>
-	);
+    const { state } = useContext(UiContext);
+
+    return (
+        <MeetingContainer>
+            <section
+                role="img"
+                className="landing"
+                style={{ backgroundImage: `url(${currentMeetup && currentMeetup?.image})` }}
+            >
+                <h1>{currentMeetup?.title}</h1>
+            </section>
+            <section className="content-container">
+                <section className="location-time">
+                    <code>{currentMeetup && currentMeetup?.time}</code>
+                    <h2>{currentMeetup?.location}</h2>
+                    <MeetupTagsContainer>
+                        {currentMeetup?.tag.map((tag) => (
+                            <TagChip key={nanoid()} text={tag} />
+                        ))}
+                    </MeetupTagsContainer>
+                </section>
+                <section className="interact-section">
+                    <MeetupBtnsContainer
+                        id={currentMeetup?.id}
+                        date={currentMeetup && currentMeetup.time}
+                    />
+                </section>
+            </section>
+            {state.showEditDeleteModal && <EditMeetupModal currentMeetup={currentMeetup} />}
+        </MeetingContainer>
+    );
 }
 
 export default Meetup;
 
 const MeetingContainer = styled.section`
-	.landing {
-		position: relative;
-		min-height: 200px;
-		background-size: cover;
-		background-position: center;
-		border-radius: ${props => props.theme.borderRadius};
-		h1 {
-			font-size: 2rem;
-			position: absolute;
-			bottom: 2rem;
-			left: 2rem;
-		}
-	}
+    .landing {
+        position: relative;
+        min-height: 200px;
+        background-size: cover;
+        background-position: center;
+        border-radius: ${(props) => props.theme.borderRadius};
+        h1 {
+            font-size: 2rem;
+            position: absolute;
+            bottom: 2rem;
+            left: 2rem;
+        }
+    }
 
-	.content-container {
-		margin-top: 1rem;
-		padding: 1rem;
-		min-height: 200px;
-		border-radius: ${props => props.theme.borderRadius};
-		background-color: ${props => props.theme.cardBgColor};
+    .content-container {
+        margin-top: 1rem;
+        padding: 1rem;
+        min-height: 200px;
+        border-radius: ${(props) => props.theme.borderRadius};
+        background-color: ${(props) => props.theme.cardBgColor};
 
-		display: flex;
-		justify-content: space-between;
-	}
+        display: flex;
+        justify-content: space-between;
+    }
 `;
 
 const MeetupTagsContainer = styled.div`
-	display: flex;
+    display: flex;
 `;
