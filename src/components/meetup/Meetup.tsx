2f85c778bafae2b61ac38b771564364ed43e0504
krispad-dev <kristofer.padoan@icloud.com>
2022-01-28 10:35:17 +0100

    Meetup css bug fixes and added extra startup meetups

--- src/components/meetup/Meetup.tsx
+++ src/components/meetup/Meetup.tsx
@@ -6,72 +6,69 @@ import { nanoid } from 'nanoid';
 import TagChip from '../globals/TagChip';
 
 interface Props {
-    currentMeetup: Meeting;
+	currentMeetup: Meeting;
 }
 
 function Meetup({ currentMeetup }: Props) {
-    
-
-    return (
-        <MeetingContainer>
-            <section
-                role="img"
-                className="landing"
-                style={{ backgroundImage: `url(${currentMeetup && currentMeetup?.image})` }}
-            >
-                <h1>{currentMeetup?.title}</h1>
-            </section>
-            <section className="content-container">
-                <section className="location-time">
-                   <code>{ currentMeetup && currentMeetup?.time}</code> 
-                    <h2>{currentMeetup?.location}</h2>
-                    <MeetupTagsContainer>
-                    {currentMeetup?.tag.map((tag) => (
-                        <TagChip key={nanoid()} text={tag} />
-                    ))}
-                    </MeetupTagsContainer>
-                </section>
-                <section className="interact-section">
-
-                <MeetupBtnsContainer id={currentMeetup?.id} date={currentMeetup && currentMeetup.time} />
-                </section>
-            </section>
-        </MeetingContainer>
-    );
+	return (
+		<MeetingContainer>
+			<section
+				role='img'
+				className='landing'
+				style={{ backgroundImage: `url(${currentMeetup && currentMeetup?.image})` }}
+			>
+				<h1>{currentMeetup?.title}</h1>
+			</section>
+			<section className='content-container'>
+				<section className='location-time'>
+					<code>{currentMeetup && currentMeetup?.time}</code>
+					<h2>{currentMeetup?.location}</h2>
+					<MeetupTagsContainer>
+						{currentMeetup?.tag.map(tag => (
+							<TagChip key={nanoid()} text={tag} />
+						))}
+					</MeetupTagsContainer>
+				</section>
+				<section className='interact-section'>
+					<MeetupBtnsContainer
+						id={currentMeetup?.id}
+						date={currentMeetup && currentMeetup.time}
+					/>
+				</section>
+			</section>
+		</MeetingContainer>
+	);
 }
 
 export default Meetup;
 
 const MeetingContainer = styled.section`
-    .landing {
-        position: relative;
-        min-height: 200px;
-        background-size: cover;
-        background-position: center;
-        border-radius: ${(props) => props.theme.borderRadius};
-        h1 {
-            font-size: 2rem;
-            position: absolute;
-            bottom: 2rem;
-            left: 2rem;
-        }
-    }
+	.landing {
+		position: relative;
+		min-height: 200px;
+		background-size: cover;
+		background-position: center;
+		border-radius: ${props => props.theme.borderRadius};
+		h1 {
+			font-size: 2rem;
+			position: absolute;
+			bottom: 2rem;
+			left: 2rem;
+		}
+	}
 
-    .content-container {
-        margin-top: 1rem;
-        padding: 1rem;
-        min-height: 200px;
-        border-radius: ${(props) => props.theme.borderRadius};
-        background-color: ${(props) => props.theme.cardBgColor};
+	.content-container {
+		margin-top: 1rem;
+		padding: 1rem;
+		min-height: 200px;
+		border-radius: ${props => props.theme.borderRadius};
+		background-color: ${props => props.theme.cardBgColor};
 
-        display: flex;
-        justify-content: space-between;
-    }
+		display: flex;
+		justify-content: space-between;
+	}
 `;
 
-
 const MeetupTagsContainer = styled.div`
-display: flex;
-
-
-`
+	display: flex;
+`;
