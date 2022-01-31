b70d7b0ec5eacaa223c9ecd2e0400dda893889e6
krispad-dev <kristofer.padoan@icloud.com>
2022-01-31 08:16:52 +0100

    added new test to Search

--- src/components/globals/test/Search.test.tsx
+++ src/components/globals/test/Search.test.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import SwitchComponent from '../SwitchComponent'
 import Search from '../Search'
 
 
-describe('TagChip component', () => {
+describe('Search component', () => {
 
 	it('should render', () => {
 		render(<Search />);
+	});
+
+	it('should have placeholder text', () => {
+		render(<Search />);
+
+		const inputField = screen.getByPlaceholderText(/sök på taggar/i)
+		expect(inputField).toBeInTheDocument()
 	});
 	
 
