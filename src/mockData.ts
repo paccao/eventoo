c922c8bc66265fb84f62dc44113a2f58104e4754
Adam-Bjurhede <adambjurhede@gmail.com>
2022-01-31 15:55:45 +0100

    changed mock-id to static

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,10 +1,9 @@
-import { nanoid } from 'nanoid';
-
 export const meetups = [
     {
-        id: nanoid(),
+        id: '1',
         title: 'Lördag på landet',
-        description: 'Träffen för alla bilnördar! Kom som du är, det bjuds på kaffe korv och turbo. Gratis däck för alla som vill elda',
+        description:
+            'Träffen för alla bilnördar! Kom som du är, det bjuds på kaffe korv och turbo. Gratis däck för alla som vill elda',
         tag: ['outdoors', 'bilar'],
         time: '2023-01-28 15:01',
         isOnline: false,
@@ -34,9 +33,10 @@ export const meetups = [
     },
 
     {
-        id: nanoid(),
+        id: '2',
         title: 'Sockerkaka hos gpa',
-        description: 'Ta med dig sockerkaka och ett glatt humör. Kaffet måste dock drickas med sockerbit och kopparna måste vara små. Va beredd på gamla anekdoter från tidig 60-tal',
+        description:
+            'Ta med dig sockerkaka och ett glatt humör. Kaffet måste dock drickas med sockerbit och kopparna måste vara små. Va beredd på gamla anekdoter från tidig 60-tal',
         tag: ['fika', 'bakelse'],
         time: '2022-01-29 15:01',
         isOnline: false,
@@ -53,9 +53,10 @@ export const meetups = [
         ],
     },
     {
-        id: nanoid(),
+        id: '3',
         title: 'Game night',
-        description: 'Nu kör vi magic! Gröna och röda lekar onskas. Denna tillställning ska ghå långsamt och räknar med ett pas på ett dygn. Vinnaren tar hem äran.',
+        description:
+            'Nu kör vi magic! Gröna och röda lekar onskas. Denna tillställning ska ghå långsamt och räknar med ett pas på ett dygn. Vinnaren tar hem äran.',
         tag: ['fika'],
         time: '2023-01-27 15:01',
         isOnline: true,
@@ -72,11 +73,12 @@ export const meetups = [
         ],
     },
     {
-        id: nanoid(),
+        id: '4',
         title: 'Räkjostillverkning med greger',
-        description: 'OBS, sista upplagan av detta inte så populära evenemang. Här träffas vi och bjuder varandra på räkcocktail som när den var som bäst.. (på 80-talet)',
+        description:
+            'OBS, sista upplagan av detta inte så populära evenemang. Här träffas vi och bjuder varandra på räkcocktail som när den var som bäst.. (på 80-talet)',
         tag: ['food', 'gourme'],
-        time: '2023-01-20 15:01',
+        time: '2023-08-20 15:01',
         isOnline: true,
         location: 'Göteborg',
         timeStamp: 1643097488,
@@ -92,11 +94,12 @@ export const meetups = [
     },
 
     {
-        id: nanoid(),
+        id: '5',
         title: 'Arcade Night!',
-        description: 'Ta med hinken med mynt, här ska det spelas spel. Den som vinner Sega Rally får 10 000 kronor i enkronor. Glöm inte handspriten.',
+        description:
+            'Ta med hinken med mynt, här ska det spelas spel. Den som vinner Sega Rally får 10 000 kronor i enkronor. Glöm inte handspriten.',
         tag: ['spel', 'liten grupp', 'arcade'],
-        time: '2023-01-20 15:01',
+        time: '2023-07-20 15:01',
         isOnline: true,
         location: 'Berlin',
         timeStamp: 1643097488,
@@ -111,11 +114,12 @@ export const meetups = [
         ],
     },
     {
-        id: nanoid(),
+        id: '6',
         title: 'Meercat spaning',
-        description: 'Kom och möts i Lasses minibuss.  Lån av kikare erbjuds  där vi skall spana på naturens egna spanare. Lite grupp på minst 10p.',
+        description:
+            'Kom och möts i Lasses minibuss.  Lån av kikare erbjuds  där vi skall spana på naturens egna spanare. Lite grupp på minst 10p.',
         tag: ['fika', 'slow', 'djur'],
-        time: '2023-01-20 15:01',
+        time: '2023-03-20 15:01',
         isOnline: true,
         location: 'Skåne',
         timeStamp: 1643097488,
@@ -135,5 +139,5 @@ export const user = {
     name: 'kalle',
     id: '1',
     isAdmin: false,
-    bookedMeetups: [],
+    bookedMeetups: ['2', '6'],
 };
